e4d5d8b20ba50a291f76722cf58a6162883f6499
ninjagrrrl <parallaxis@posteo.com>
2025-05-20 14:40:25 +0200

    editCreateClient

--- src/lib/supabase/client.ts
+++ src/lib/supabase/client.ts
@@ -1,6 +1,5 @@
 import { createClient as createSupabaseClient } from "@supabase/supabase-js";
 import type { Database } from "../../types/supabase";
-// Create a single supabase client for interacting with your database
 
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
 const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
