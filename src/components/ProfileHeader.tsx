c0e86fb9a333b56e25dbfd58e281b2041af555a5
elmin-hasanov <elmin2203@gmail.com>
2025-05-20 14:42:20 +0200

    Profile Header

--- src/components/ProfileHeader.tsx
+++ src/components/ProfileHeader.tsx
@@ -1,13 +1,13 @@
 import logo from '@/assets/logo.svg';
 import { Heart } from 'lucide-react';
 
-function Header() {
+function ProfileHeader() {
   console.log('header');
   return (
     <header className="flex items-center justify-between p-6">
       <div className="flex items-center justify-center gap-3">
         <img src={logo} alt="toktok-logo" />
-        <h2 className="font-bold text-2xl">TokTok</h2>
+        <h2 className="font-bold text-2xl"> Profile</h2>
       </div>
       <div className="flex items-center justify-center">
         <Heart className="w-7 h-7" />
@@ -16,4 +16,4 @@ function Header() {
   );
 }
 
-export default Header;
+export default ProfileHeader;
